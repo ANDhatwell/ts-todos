9e01de5d41689ed2b549a877bd59bdcda04d6322
Caroline Hatwell <caroline.hatwell@and.digital>
2022-12-19 20:47:40 +0000

    ts app

---
+++ src/components/ToDoList.test.tsx
@@ -0,0 +1,6 @@
+import { Screen } from "@testing-library/react"
+describe('ToDoList', ()=>{
+    it('should render todos', ()=>{
+        expect(true).toBe(true)
+    })
+})
---
+++ src/components/ToDoListItem.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react"
+import { TodoListItem } from "./ToDoListItem"
+
+describe('ToDoListItem', ()=>{
+    const toggleTodo = jest.fn()
+        it('should render uncomplete todo', ()=>{
+        render(<TodoListItem todo={{text:"Get a haircut", complete: false}} toggleTodo={toggleTodo}/>)
+        expect(screen.getByText(/get a haircut/i)).toBeInTheDocument()
+        const checkbox = screen.getByRole('checkbox')
+        expect(checkbox).not.toBeChecked()
+    })
+
+    it('should render complete todo', ()=>{
+        render(<TodoListItem todo={{text:"Get a haircut", complete: true}} toggleTodo={toggleTodo}/>)
+        expect(screen.getByText(/get a haircut/i)).toBeInTheDocument()
+        const checkbox = screen.getByRole('checkbox')
+        expect(checkbox).toBeChecked()
+    })
+
+    it('should complete todo', ()=>{
+    const todo: Todo = {text: 'Go to the post office', complete: true}
+    render(<TodoListItem todo={todo} toggleTodo={toggleTodo}/>)
+    expect(screen.getByText(/go to the post office/i)).toBeInTheDocument()
+    const checkbox = screen.getByRole('checkbox')
+    expect(checkbox).not.toBeChecked()
+})
+})
